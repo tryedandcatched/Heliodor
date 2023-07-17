4f44fa1cb8e1847f176b11d10226f0cbffd7d9fa
Joxannn <jonasz.patelski@gmail.com>
2023-07-17 16:05:51 +0200

    dsakhldhsahdklas

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const prompt = require('prompt');
 const fs = require('fs');
-const { Client } = require('discord.js-selfbot-v13');
-
+const { Client, RichPresence, DiscordRPCServer } = require('discord.js-selfbot-v13');
+const Discord = require('discord.js-selfbot-v13');
 let token = "";
+
+
+
 function getToken(){
 	prompt.start();
 	prompt.get([{
@@ -26,12 +29,13 @@ function getToken(){
 	});
 }
 
-fs.readFile('token.txt', function (err, data) {
+fs.readFile('token.txt', 'utf8', function (err, data) {
+
 	if (err) {
 		getToken(); 
 		return;
 	}
- 	if (data) {
+ 	if (data != "") {
 		console.log("Token Restored Successfully!");
 		token = data;
 		runClient();
@@ -39,27 +43,13 @@ fs.readFile('token.txt', function (err, data) {
 		getToken(); 
 	}
 });
-
 const client = new Client({
-	fetchAllMembers: false,
-	restTimeOffset: 0,
-	restWsBridgetimeout: 100,
-	shards: "auto",
-	allowedMentions: {
-	  parse: [],
-	  repliedUser: true,
-	},
-	presence: {
-	  activity: {
-		name: `Wasi Music`,
-		type: "LISTENING",
-	  },
-	  status: "online"
-	}
+	checkUpdate: false,
 });
 
 function runClient() {
-	if(token) {
+	if(token != "") {
+		
 		client.login(token);
 	} else {
 		console.error("Invalid Token!");
@@ -69,4 +59,43 @@ function runClient() {
 
 client.on('ready', async () => {
 	console.log(`${client.user.username} is ready!`);
+	
+client.user.setSamsungActivity('com.supercell.clashroyale', 'START');
+});
+client.on('messageCreate', async (msg) => {
+	//if (msg.author.bot) return;
+	//if (!['796809537368621086', client.user.id].includes(msg.author.id)) return;
+	//if (!msg.content) return;
+	//if (!msg.content.startsWith(`.`)) return;
+	const args = msg.content.trim().slice(1).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if (command == "test") {
+		msg.delete();
+		msg.channel.send("**El Bot Est Functionele** ✅")
+		console.log("El Bot Est Functionele ✅")
+	}
+
+	if (command == "ukrytawiadomosc") {
+		
+		msg.channel.send(args[1]+"**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍**‍ " + args[0])
+
+	}
+	if (command == "moyai") {
+		msg.delete();
+		for(let i = 0; i < args[0]; i++){
+			msg.channel.send({ content: `🗿 🗿 🗿 🗿 🗿 🗿 🗿 🗿 🗿 🗿 🗿`})
+		  }
+		}
+	if (command == "ocw"){
+		const rand = Math.random() < 0.5
+		if (rand == true) {
+			msg.channel.send({ content: `**Oskar Czy Wasiluk**\n \`\`\`Oskar 🤑\`\`\`\n Stawiales na: **` + args[0] +`**`})
+		} else {
+			msg.channel.send({ content: `**Oskar Czy Wasiluk**\n \`\`\`Wasiluk 👹\`\`\`\n Stawiales na: **` + args[0] +`**`})
+		}
+
+	}
+	   	
+
 });
